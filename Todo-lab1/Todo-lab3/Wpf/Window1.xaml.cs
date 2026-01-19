9d10d79e588a98553a15cf6b28db01675610dfbe
Nikita21154 <nik.yegorov.2007@gmail.com>
2026-01-19 20:36:42 +0300

    Add files via upload

---
+++ Todo-lab1/Todo-lab3/Wpf/Window1.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+
+namespace Wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        UserRepository UR = new UserRepository();
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+          
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void TextBox_TextChanged_3(object sender, TextChangedEventArgs e)
+        {
+            
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow MainWindow = new MainWindow();
+            MainWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (proverka.ValidateInputs(Name, Email, PassWord, RePassWord))
+            {
+                if (UR.UserRegistration(Name.Text, PassWord.Text.Trim(), Email.Text))
+                {
+                    Main_empty main_Empty = new Main_empty();
+                    main_Empty.Show();
+                    this.Close();
+                }
+            }
+
+        }
+    }
+}
